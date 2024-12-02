1f998d2140e9ef90c2cf685cf7948aa04a1b1525
Jorge Bravo <jbravoveintemilla@gmail.com>
2024-12-02 17:08:49 -0500

    Avance Semana 14

--- app/src/main/java/com/example/dam_sem11_proyectobravo/MainActivity.java
+++ app/src/main/java/com/example/dam_sem11_proyectobravo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.dam_sem11_proyectobravo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        // Definir el evento click sobre el botón Login
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
